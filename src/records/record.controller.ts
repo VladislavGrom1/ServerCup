0c8775a22bedd75ef529f85c4eaf6daef15cde73
VladislavGrom <vaganov.vladislav2015@yandex.ru>
2024-05-29 17:13:02 +0300

    Заготовка контроллера + DTO

--- src/records/record.controller.ts
+++ src/records/record.controller.ts
@@ -1,17 +1,46 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Param, HttpCode, HttpStatus, Body, Delete, Patch } from '@nestjs/common';
 import { RecordService } from './record.service';
+import { InsertOneRecord } from './dtos/insert-one-record.dto';
+import { UpdateOneRecord } from './dtos/update-one-record.dto';
 
 @Controller('records')
 export class RecordController {
   constructor(private readonly recordService: RecordService) {}
 
-  @Get('get')
-  get(): string {
+
+
+  @Get('getOneRecord:id')
+  getOneRecord(@Param('id') id: number): string {
     return "";
   }
 
-  @Post('set')
-  set(): string {
+  @Get('getAllRecords')
+  getAllRecords(): string {
     return "";
   }
+
+  @Post('insertOneRecord')
+  @HttpCode(HttpStatus.CREATED)
+  insertOneRecord(@Body() insert: InsertOneRecord){
+
+  }
+
+  @Delete('deleteOneRecord:id')
+  @HttpCode(204)
+  deleteOneRecord(@Param('id') id: number) {
+    
+  }
+
+  @Delete('deleteAllRecords')
+  @HttpCode(204)
+  deleteAllRecords(){
+
+  }
+
+  @Patch(":id")
+  @HttpCode(204)
+  updateOneRecord(@Param('id') id: number, @Body() update: UpdateOneRecord){
+    
+  }
+
 }
